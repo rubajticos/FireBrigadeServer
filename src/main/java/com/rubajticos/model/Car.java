b7c6ec8805bef1219e4562a80010ea37d85e6dce
mrubajczyk <rubajticos@gmail.com>
2018-02-01 20:12:50 +0100

    Mapping Car table to Car class

---
+++ src/main/java/com/rubajticos/model/Car.java
@@ -0,0 +1,26 @@
+package com.rubajticos.model;
+
+public class Car {
+
+    private int id;
+    private String model;
+    private String operationalNumbers;
+    private String plates;
+    private String type;
+    private int water;
+    private int fireBrigadeId;
+
+    public Car() {
+        this(-1, null, null, null, null, -1, -1);
+    }
+
+    public Car(int id, String model, String operationalNumbers, String plates, String type, int water, int fireBrigadeId) {
+        this.id = id;
+        this.model = model;
+        this.operationalNumbers = operationalNumbers;
+        this.plates = plates;
+        this.type = type;
+        this.water = water;
+        this.fireBrigadeId = fireBrigadeId;
+    }
+}
