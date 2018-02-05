6f7b10ca1cb93c5371d6ff9fb760346e1efbcc1f
mrubajczyk <rubajticos@gmail.com>
2018-02-05 22:56:22 +0100

    Add interaface and class for IncidentDAO

---
+++ src/main/java/com/rubajticos/database/IncidentDAO.java
@@ -0,0 +1,29 @@
+package com.rubajticos.database;
+
+import com.rubajticos.database.interfaces.InterfaceIncidentDAO;
+import com.rubajticos.model.*;
+
+import java.util.Date;
+import java.util.HashSet;
+
+public class IncidentDAO implements InterfaceIncidentDAO {
+    @Override
+    public int insertIncident(String type, Date date, String city, String decription, HashSet<Firefighter> firefighters, HashSet<FireBrigade> fireBrigades, HashSet<Equipment> equipments, HashSet<Car> cars) {
+        return 0;
+    }
+
+    @Override
+    public boolean updateIncident(int id, String type, Date date, String city, String decription, HashSet<Firefighter> firefighters, HashSet<FireBrigade> fireBrigades, HashSet<Equipment> equipments, HashSet<Car> cars) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteIncident(int id) {
+        return false;
+    }
+
+    @Override
+    public HashSet<Incident> selectIncidents(int FirebrigadeId) {
+        return null;
+    }
+}
