06bb5cdcad8557f646275833ba2fa03120507445
mrubajczyk <rubajticos@gmail.com>
2018-03-09 22:01:31 +0100

    created User create test

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -1,2 +1,28 @@
+import com.firebrigadeserver.entity.FireBrigade;
+import com.firebrigadeserver.entity.User;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class UserTest {
+
+
+    @Test
+    public void createUserTest() {
+        User us = new User();
+        us.setUserId(1);
+        us.setUsername("testUser");
+        us.setPassword("testPassword");
+
+        FireBrigade fb = new FireBrigade();
+        fb.setIdFireBrigade(1);
+        fb.setName("testFirebrigade");
+
+        us.setFireBrigade(fb);
+
+        Assert.assertNotNull(us.getUsername());
+        Assert.assertNotNull(us.getPassword());
+        Assert.assertNotNull(us.getUserId());
+        Assert.assertNotNull(us.getFireBrigade());
+
+    }
 }
