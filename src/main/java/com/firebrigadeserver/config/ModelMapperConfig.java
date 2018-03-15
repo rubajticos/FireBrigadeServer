02ab6399fe0ca7db60aa1bc830885827768dc74d
mrubajczyk <rubajticos@gmail.com>
2018-03-16 00:59:01 +0100

    Added UserMapper Bean

--- src/main/java/com/firebrigadeserver/config/ModelMapperConfig.java
+++ src/main/java/com/firebrigadeserver/config/ModelMapperConfig.java
@@ -1,5 +1,6 @@
 package com.firebrigadeserver.config;
 
+import com.firebrigadeserver.dto.mapper.UserMapper;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,5 +11,10 @@ public class ModelMapperConfig {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public UserMapper userMapper() {
+        return new UserMapper();
     }
 }
--- src/test/java/dto/mapper/UserMapperTest.java
+++ src/test/java/dto/mapper/UserMapperTest.java
@@ -6,6 +6,9 @@ import com.firebrigadeserver.entity.User;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserMapperTest {
 
     private UserMapper userMapper = new UserMapper();
@@ -19,24 +22,79 @@ public class UserMapperTest {
         user.setUsername("test");
         user.setPassword("password");
 
-        UserDTO userDTO = userMapper.EntityToDto(user);
+        UserDTO userDTO = userMapper.entityToDto(user);
 
-        Assert.assertEquals((Integer) user.getUserId(), userDTO.getId());
+        Assert.assertEquals((Integer) user.getUserId(), userDTO.getUserId());
         Assert.assertEquals(user.getUsername(), userDTO.getUsername());
         Assert.assertEquals(user.getPassword(), userDTO.getPassword());
     }
 
     @Test
+    public void entityToDtoNotNullTest() {
+
+        User user = new User();
+        user.setUserId(1);
+        user.setUsername("test");
+        user.setPassword("password");
+
+        Assert.assertNotNull(userMapper.entityToDto(user));
+
+    }
+
+    @Test
+    public void entityListToDtoListTest() {
+        List<User> users = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            User user = new User();
+            user.setUserId(i);
+            user.setUsername("test" + i);
+            user.setPassword("password" + i);
+            users.add(user);
+        }
+
+        List<UserDTO> dtoUsers = userMapper.entityListToDtoList(users);
+
+        for (int i = 0; i < dtoUsers.size(); i++) {
+            Assert.assertEquals((Integer) users.get(i).getUserId(), dtoUsers.get(i).getUserId());
+            Assert.assertEquals(users.get(i).getUsername(), dtoUsers.get(i).getUsername());
+            Assert.assertEquals(users.get(i).getPassword(), dtoUsers.get(i).getPassword());
+        }
+
+    }
+
+    @Test
     public void DtoToEntityTest() {
         UserDTO userDTO = new UserDTO();
-        userDTO.setId(1);
+        userDTO.setUserId(1);
         userDTO.setUsername("ttt");
         userDTO.setPassword("pass");
 
-        User user = userMapper.DtoToEntity(userDTO);
+        User user = userMapper.dtoToEntity(userDTO);
 
-        Assert.assertEquals(userDTO.getId(), (Integer) user.getUserId());
+        Assert.assertEquals(userDTO.getUserId(), (Integer) user.getUserId());
         Assert.assertEquals(userDTO.getUsername(), user.getUsername());
         Assert.assertEquals(userDTO.getPassword(), user.getPassword());
+    }
+
+    @Test
+    public void dtoListToEntityListTest() {
+        List<UserDTO> dtoUsers = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            UserDTO userDTO = new UserDTO();
+            userDTO.setUserId(1);
+            userDTO.setUsername("ttt");
+            userDTO.setPassword("pass");
+            dtoUsers.add(userDTO);
+        }
+
+        List<User> entityUsers = userMapper.dtoListToEntityList(dtoUsers);
+
+        for (int i = 0; i < entityUsers.size(); i++) {
+            Assert.assertEquals(dtoUsers.get(i).getUserId(), (Integer) entityUsers.get(i).getUserId());
+            Assert.assertEquals(dtoUsers.get(i).getUsername(), entityUsers.get(i).getUsername());
+            Assert.assertEquals(dtoUsers.get(i).getPassword(), entityUsers.get(i).getPassword());
+        }
+
+
     }
 }
