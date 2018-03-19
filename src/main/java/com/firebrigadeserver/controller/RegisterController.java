b536ac18cf1274175adb1e99d530230b7b20130e
mrubajczyk <rubajticos@gmail.com>
2018-03-19 14:21:07 +0100

    modified register controller for return ResponseEntity and support error codes

--- src/main/java/com/firebrigadeserver/controller/RegisterController.java
+++ src/main/java/com/firebrigadeserver/controller/RegisterController.java
@@ -3,6 +3,7 @@ package com.firebrigadeserver.controller;
 import com.firebrigadeserver.dto.UserDTO;
 import com.firebrigadeserver.dto.mapper.UserMapper;
 import com.firebrigadeserver.entity.User;
+import com.firebrigadeserver.repositories.UserRepository;
 import com.firebrigadeserver.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -19,17 +20,34 @@ public class RegisterController {
     private IUserService userService;
 
     @Autowired
+    private UserRepository userRepository;
+
+
+    @Autowired
     private UserMapper userMapper;
 
     @RequestMapping(value = "register", method = RequestMethod.POST)
-    public ResponseEntity<Void> register(@RequestBody UserDTO userDto) {
+    public ResponseEntity register(@RequestBody UserDTO userDto) {
+        UserDTO returnUserDTO = null;
         try {
             User user = userMapper.dtoToEntity(userDto);
-            userService.addUser(user);
+            if (userService.addUser(user)) {
+                user = userRepository.findByUsername(user.getUsername());
+                returnUserDTO = userMapper.entityToDto(user);
+                return ResponseEntity
+                        .status(HttpStatus.OK)
+                        .body(returnUserDTO);
+            } else {
+                return ResponseEntity
+                        .status(HttpStatus.CONFLICT)
+                        .body(returnUserDTO);
+            }
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        return new ResponseEntity<Void>(HttpStatus.OK);
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(returnUserDTO);
     }
 
 }
